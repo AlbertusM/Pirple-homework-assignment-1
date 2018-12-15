eea1879ed351788ef27a7c9d6029fa4a2e58a332
Bertie <abmeiring@gmail.com>
2018-12-15 20:52:33 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# variables for audio meta data with intergers, floats and strings
+genre = "Rock"
+YearReleased = 1980
+duration = 3.45
+title = "rockheads"
+TrackNumber = 1
+ArtistName = "Bonjovi"
+AlbumName = "Best of the 80's"
+FileFormat = "mp3"
+# printing to console all variables
+print(genre)
+print(YearReleased)
+print(duration)
+print(title)
+print(TrackNumber)
+print(ArtistName)
+print(AlbumName)
+print(FileFormat)
